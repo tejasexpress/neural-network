78a78f5d572c997eed8cf7d936f0355913db3a00
tejasexpress <45305155+tejasexpress@users.noreply.github.com>
2024-01-16 10:27:22 +0530

    Update neuralnet.py

--- neuralnet.py
+++ neuralnet.py
@@ -5,7 +5,7 @@ def sigmoid(z):
         # The sigmoid function.
         return 1.0/(1.0+np.exp(-z))
 
-    def sigmoid_prime(z):
+def sigmoid_prime(z):
         # Derivative of the sigmoid function.
         return sigmoid(z)*(1-sigmoid(z))
 
